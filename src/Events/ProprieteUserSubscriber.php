136e79c2cf61f7f2ae11f73ee23f7b0a58341ea5
byab1 <yemakarios70@gmail.com>
2021-05-26 23:17:10 +0000

    Initial commit

--- src/Events/ProprieteUserSubscriber.php
+++ src/Events/ProprieteUserSubscriber.php
@@ -5,7 +5,6 @@ namespace App\Events;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use ApiPlatform\Core\EventListener\EventPriorities;
-use App\Entity\Agence;
 use App\Entity\Propriete;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Security;
